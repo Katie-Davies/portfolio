8623846ba0abb3af90adce5dbd8a1e95b87fdd83
Katie Davies <millerkatie1990@gmail.com>
2024-05-01 14:54:19 +1200

    update z index

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -14,7 +14,7 @@ function Nav() {
   }
 
   return (
-    <div className="fixed flex justify-between w-full bg-white">
+    <div className="fixed flex justify-between w-full bg-white z-50">
       <h1 className="md:m-3 sm:m-2 lg:m-4 px-3">Katie Davies </h1>
       <div className="flex justify-around">
         <button
