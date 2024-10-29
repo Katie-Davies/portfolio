a074ba4cabeecf92e731fe4c8dca7524b9a00a0a
Katie Davies <millerkatie1990@gmail.com>
2024-10-29 19:17:59 +1300

    add blurb

--- client/components/Playbased.tsx
+++ client/components/Playbased.tsx
@@ -6,20 +6,23 @@ function Prompter() {
       <div className="prompter-bg flex content-start justify-center flex-wrap ml-5">
         <div className="flex content-center flex-wrap w-2/3 justify-center mt-10 mr-5">
           <ul>
-            <li className="font-bold mb-3 text-2xl">Prompter</li>
+            <li className="font-bold mb-3 text-2xl">PlayBased</li>
             <li>
-              <strong>Status:</strong> Completed
+              <strong>Status:</strong> Development V1
             </li>
             <li>
-              <strong>Tech: </strong>React | React-Router | Node | Express |
-              Typescript | sqlite3 | Auth0
+              <strong>Tech: </strong>React | React-Router | .NET | Entity
+              Framework Core | Typescript | Azure SQL Databse | Auth0 | Azure
+              Blob Storage
             </li>
 
             <li className="mt-3">
-              A Friday group project based on Reddit to create a fun place for
-              people to find icebreaker questions and post their response. This
-              was our first project implementing Auth0 to allow users to have
-              their own accounts.
+              A website to provide parents a range of different activities to do
+              with their children. They are categorised by age group and provide
+              a list of materials and instructions. The user is able to search
+              by materials and can save activities to their favourites. This was
+              my first attempt at using .net, Azure Sql DB and Azure Blob
+              Storage.
             </li>
           </ul>
         </div>
